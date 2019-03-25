eb76f3fdfc41f22fff44a3890a793416b50f0062
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-03-25 22:11:11 +0100

    Creata funzione aggiungiStella. Da testare, ma in teoria dovrebbe andare

--- Planetarium/src/planetarium/Planetarium.java
+++ Planetarium/src/planetarium/Planetarium.java
@@ -2,12 +2,12 @@ package planetarium;
 import java.util.Scanner;
 
 public class Planetarium {
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		System.out.print("Inserisci il nome del sistema solare: ");
 		String nome = sc.nextLine();
-		Sistema_Stellare ss = new Sistema_Stellare(nome);
+		SistemaStellare ss = new SistemaStellare(nome);
 		String cmnd;
 		System.out.println("Digita aiuto per ricevere aiuto");
 		boolean end = false;
@@ -15,11 +15,16 @@ public class Planetarium {
 			System.out.println("Che cosa vuoi fare? ");
 			cmnd = sc.nextLine();
 			switch(cmnd) {
-			 case "aiuto":
-				 aiuto();
-				 break;
+			case "aiuto":
+				aiuto();
+				break;
+			case "aggiungi stella":
+				aggiungiStella(ss);
+				break;
 			}
 		}
+		
+		sc.close();
 	}
 	/**
      * @return
@@ -35,5 +40,30 @@ public class Planetarium {
 		System.out.println("rimuovi satellite: per rimuovere un satellite");
 		System.out.println("calcola rotta: per calcolare la rotta di un corpo celeste");
 		System.out.println("collidono: per vedere se 2 corpi potrebbero collidere");
+	}
+	
+	public static void aggiungiStella(SistemaStellare ss) {
+		System.out.println("Inserisci il nome della stella: ");
+		String nome = sc.nextLine();
+		String codice;
+		
+		System.out.println("Vuoi assegnare un codice manualmente? S/[N]: ");
+		boolean codiceMan = sc.nextLine().equals("S")? true : false;
+		if(codiceMan) {
+			System.out.println("Inserisci il codice della stella: ");
+			codice = sc.nextLine();
+		} else codice = "RND" + Math.floor(Math.random()*100000);
+		
+		System.out.println("Inserisci il peso della stella: ");
+		int peso = Integer.parseInt(sc.nextLine());
+		
+		System.out.println("Inserisci le coordinate della stella (x,y): ");
+		int coordX = sc.nextInt();
+		int coordY = sc.nextInt();
+		Punto punto = new Punto(coordX, coordY);
+		
+		ss.aggiungiStella(new Stella(nome, codice, peso, punto));
+		
+		
 	}
 }
