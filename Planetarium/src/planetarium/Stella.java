e6d0db1bf51b9b8dbdefdb3ec6823179feb65f18
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-03-26 16:01:38 +0100

    Codice è String

--- Planetarium/src/planetarium/Stella.java
+++ Planetarium/src/planetarium/Stella.java
@@ -20,7 +20,7 @@ public class Stella extends CorpoCeleste{
      * @param codice
      * @return
      */
-	public boolean rimuoviPian(int codice) {
+	public boolean rimuoviPian(String codice) {
 		boolean isRemove = false;
     	for(Pianeta pianeta: pianeti) {
     		if(pianeta.codice.equals(codice)){
@@ -35,7 +35,7 @@ public class Stella extends CorpoCeleste{
      * @param codice
      * @return
      */
-	public Pianeta getPianeta(int codice) {
+	public Pianeta getPianeta(String codice) {
 		for(Pianeta pianeta: pianeti) {
     		if(pianeta.codice.equals(codice))
     			return pianeta;
