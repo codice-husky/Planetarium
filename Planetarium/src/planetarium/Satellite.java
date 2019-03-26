6f85085916f0fe73d472cfb0929564b5041c8ac6
Federico Mason <f.mason@studenti.unibs.it>
2019-03-26 15:12:22 +0100

    tolto il distanzaPianeta

--- Planetarium/src/planetarium/Satellite.java
+++ Planetarium/src/planetarium/Satellite.java
@@ -1,23 +1,8 @@
 package planetarium;
 
 public class Satellite extends CorpoCeleste{
-	String distPianeta;
 	
 	public Satellite(String _nome, String _codice, int _peso, Punto _punto, String _distPianeta) {
 		super(_nome, _codice, _peso, _punto);
-		distPianeta = _distPianeta;
-	}
-	
-	/** 
-     * @param dist 
-     */
-	public void setDistanzaPianeta(String dist) {
-		this.distPianeta = dist;
-	}
-	/** 
-     * @return 
-     */
-	public String getDistanzaPianeta() {
-		return distPianeta;
 	}
 }
