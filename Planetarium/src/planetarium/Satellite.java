87c15e4de63a242fa06e12c35d9366f96c5f10b1
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-03-26 16:00:19 +0100

    La distanza la calcoliamo poi

--- Planetarium/src/planetarium/Satellite.java
+++ Planetarium/src/planetarium/Satellite.java
@@ -2,7 +2,7 @@ package planetarium;
 
 public class Satellite extends CorpoCeleste{
 	
-	public Satellite(String _nome, String _codice, int _peso, Punto _punto, String _distPianeta) {
+	public Satellite(String _nome, String _codice, int _peso, Punto _punto) {
 		super(_nome, _codice, _peso, _punto);
 	}
 }
