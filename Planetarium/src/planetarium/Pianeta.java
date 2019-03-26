ebf33099aa330acab71522fea2054fd0fc8f1b56
Federico Mason <f.mason@studenti.unibs.it>
2019-03-26 14:53:34 +0100

    cambiato il codice in Satellite da int a String, e addSatellite

--- Planetarium/src/planetarium/Pianeta.java
+++ Planetarium/src/planetarium/Pianeta.java
@@ -5,25 +5,29 @@ public class Pianeta extends CorpoCeleste {
 	private int distStella;
 	private LinkedList<Satellite> satelliti;
 
-	public Pianeta(String _nome, String _codice, int _peso, Punto _punto) {
+	public Pianeta(String _nome, String _codice, int _peso, Punto _punto,int _distanzaStella) {
 		super(_nome, _codice, _peso, _punto);
+		distStella = _distanzaStella;
+		satelliti = new LinkedList<Satellite>();
 	}
 
     /**
      * @return
      */
-    public void aggiungiSat() {}
+    public void aggiungiSat(Satellite satellite) {
+    	satelliti.add(satellite);
+    }
 
     /**
      * @param codice 
      */
-    public void rimuoviSat(int codice){}
+    public void rimuoviSat(String codice){}
 
     /**
      * @param codice
      * @return
      */
-    public Satellite getSat(int codice){return null;}
+    public Satellite getSat(String codice){return null;}
 
     /**
      * @param dist 
--- Planetarium/src/planetarium/Satellite.java
+++ Planetarium/src/planetarium/Satellite.java
@@ -1,22 +1,23 @@
 package planetarium;
 
 public class Satellite extends CorpoCeleste{
-	int distPianeta;
+	String distPianeta;
 	
-	public Satellite(String _nome, String _codice, int _peso, Punto _punto) {
+	public Satellite(String _nome, String _codice, int _peso, Punto _punto, String _distPianeta) {
 		super(_nome, _codice, _peso, _punto);
+		distPianeta = _distPianeta;
 	}
 	
 	/** 
      * @param dist 
      */
-	public void setDistanzaPianeta(int dist) {
+	public void setDistanzaPianeta(String dist) {
 		this.distPianeta = dist;
 	}
 	/** 
      * @return 
      */
-	public int getDistanzaPianeta() {
+	public String getDistanzaPianeta() {
 		return distPianeta;
 	}
 }
